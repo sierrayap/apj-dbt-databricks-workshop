761520cbe6f4613c88c5171b11881380cf72caa8
Richard Yeo <richard.yeo@databricks.com>
2025-09-11 07:22:53 +0000

    richrichrich

--- models/lineitem_bronze.sql
+++ models/lineitem_bronze.sql
@@ -6,4 +6,5 @@
 
 
 select *
-from stream read_files('/Volumes/apj_dbt_workshop/setup/apj-dbt-workshop/lineitem.json/')
+from stream 
+read_files('/Volumes/apj_dbt_workshop/setup/apj-dbt-workshop/lineitem.json/')
